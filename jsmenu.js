7fe894e74d93331ee5bff04b99f296c76538e4eb
Jim OConnell <resourcemedia@gmail.com>
2021-05-22 14:25:24 -0400

    Update

--- jsmenu.js
+++ jsmenu.js
@@ -218,7 +218,7 @@ for (var i = 0; i < buttonArrayLength1; i++) {
 
       // This updates the data-ButtonValue of the child
       divButtonElement.setAttribute('data-buttonValue', 'open');
-      var testXX = document.getElementById("div-" + divButtonElement);
+      var testXX = document.getElementById("dropdown-" + divButtonElement);
       testXX.style.display = "block";
       divButtonElement.value = "open";
       divButtonElement.display = "block";
