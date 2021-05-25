e86f965d41923e339e90036a5470a3ad60c45583
Jim OConnell <resourcemedia@gmail.com>
2021-05-25 15:58:10 -0400

    Update

--- jsmenu.js
+++ jsmenu.js
@@ -216,6 +216,7 @@ for (var i = 0; i < buttonArrayLength1; i++) {
     // This gets the Level array property
     var level = buttonArray[i].level;
 
+    console.log("divBarElement x " + divBarElement);
 
     divBarElement.style.fontWeight = "bold";
     divBarElement.style.textDecoration = "underline";
