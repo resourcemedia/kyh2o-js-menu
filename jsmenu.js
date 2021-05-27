848a954263858598bda95a3f42234b951eec2614
Jim OConnell <resourcemedia@gmail.com>
2021-05-27 17:00:38 -0400

    Update

--- jsmenu.js
+++ jsmenu.js
@@ -60,7 +60,6 @@ var buttonArray = [
           {level:6, name: "waterborne-pathogens",    value:"closed", display:"none", url: urlx + "indoor-6/waterborne-pathogens"},
         {level:5,   name: "pInorganics",             value:"closed", display:"none", url: urlx + "indoor-5/inorganics"},
           {level:6, name: "antimony",                value:"closed", display:"none", url: urlx + "indoor-6/antimony"},
-          {level:6, name: "arsenic",                 value:"closed", display:"none", url: urlx + "indoor-6/arsenic"}
 /*
 
         {level:4,   name: "vocs",                    value:"closed", display:"none", url: urlx + "/aluminum.html"},
