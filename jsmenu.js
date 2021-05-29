5a514a6f27e3e729b72261795279981a8ce7b652
Jim OConnell <resourcemedia@gmail.com>
2021-05-29 10:19:49 -0400

    Update

--- jsmenu.js
+++ jsmenu.js
@@ -1,4 +1,3 @@
-
 // This Removes the corresponding mobile or desk menu so there isn't duplicate code.
 
 
@@ -29,72 +28,35 @@ breakpointDisplay();
 
 
 
-// This creates a prefix so the URL in the buttonArray isn't long - can be swapped with different URL when moved
-var urlx = "https://know-your-h2o.webflow.io/";
 
-//localStorage.setItem("state", "changed");
+
+
 
 // MODULE 1 ESTABLISH ARRAY OR GET STORAGE - - - - - - - - - - - - - - - - - - - - -
 // This creates the original local storage array
 
-if (localStorage.getItem("state") === "changed") {
+if (sessionStorage.getItem("state") === "changed") {
   // Comes in as an array because it was stringifyed to going out.
- var retreiveData = localStorage.getItem("buttonArray");
+ var retreiveData = sessionStorage.getItem("buttonArray");
  var arrayParse = JSON.parse(retreiveData);
  var buttonArray = Array.from(arrayParse);
 
 }
 else {
   var buttonArray = [
-    {level:1,         name: "indoor",                  value:"open",    display:"display",  drop: true,  url: urlx + "indoor/indoor"},
-    {level:2,         name: "get-informed",            value:"closed",  display:"none",     drop: false,  url: urlx + ""},
-      {level:3,       name: "get-informed-intro",      value:"closed",  display:"none",     drop: true,  url: urlx + "indoor-2/get-informed"},
-      {level:3,       name: "contaminants",            value:"closed",  display:"none",     drop: true,  url: urlx + "indoor-3/contaminants"},
-        {level:4,     name: "primary-contaminants",    value:"closed",  display:"none",     drop: true,  url: urlx + "indoor-4/primary-contaminants"},
-          {level:5,   name: "pMicrobiological",        value:"closed",  display:"none",     drop: true,  url: urlx + "indoor-5/microbiological"},
-            {level:6, name: "bacteria",                value:"closed",  display:"none",     drop: false,  url: urlx + "indoor-6/bacteria"},
-            {level:6, name: "total-coliform-bacteria", value:"closed",  display:"none",     drop: false,  url: urlx + "indoor-6/total-coliform-bacteria"},
-            {level:6, name: "e-coli",                  value:"closed",  display:"none",     drop: false,  url: urlx + "indoor-6/e-coli"},
-            {level:6, name: "giardia-cryptosporidium", value:"closed",  display:"none",     drop: false,  url: urlx + "indoor-6/giardia-cryptosporidium"},
-            {level:6, name: "viruses",                 value:"closed",  display:"none",     drop: false,  url: urlx + "indoor-6/viruses"},
-            {level:6, name: "waterborne-pathogens",    value:"closed",  display:"none",     drop: false,  url: urlx + "indoor-6/waterborne-pathogens"},
-          {level:5,   name: "pInorganics",             value:"closed",  display:"none",     drop: true,  url: urlx + "indoor-5/inorganics"},
-            {level:6, name: "antimony",                value:"closed",  display:"none",     drop: false,  url: urlx + "indoor-6/antimony"}
-    /*
+    {level:1,         name: "indoor",       value:"closed",  display:"none", state: "+"},
+      {level:2,       name: "get-informed", value:"closed",  display:"none", state: "+"},
+        {level:3,     name: "contaminants", value:"closed",  display:"none", state: "+"}
 
-          {level:4,   name: "vocs",                    value:"closed", display:"none", url: urlx + "/aluminum.html"},
-          {level:4,   name: "socs",                    value:"closed", display:"none"},
-          {level:4,   name: "disinfectionByProducts",  value:"closed", display:"none"},
-          {level:4,   name: "radioactiveIsotopes",     value:"closed", display:"none"},
-        {level:3,     name: "secondaryContaminants",   value:"closed", display:"none"},
-          {level:4,   name: "sInorganics",             value:"closed", display:"none"},
-            {level:5, name: "aluminum",                value:"closed", display:"none", url: urlx + "/aluminum.htmlx"},
-          {level:4,   name: "conditions",              value:"closed", display:"none"},
-        {level:3,     name: "noStandardsContaminants", value:"closed", display:"none"},
-          {level:4,   name: "nMicrobiological",        value:"closed", display:"none"},
-          {level:4,   name: "nInorganics",             value:"closed", display:"none"},
-          {level:4,   name: "nOrganics",               value:"closed", display:"none"},
-          {level:4,   name: "gases",                   value:"closed", display:"none"},
-          {level:4,   name: "nConditions",             value:"closed", display:"none"},
-      {level:2,       name: "standards",               value:"closed", display:"none"},
-      {level:2,       name: "waterScienceBasics",      value:"closed", display:"none"},
-      {level:2,       name: "drinkingWaterTopics",     value:"closed", display:"none"},
-      {level:2,       name: "caseStudies",             value:"closed", display:"none"},
-    {level:1,         name: "getTested",               value:"closed", display:"none"},
-    {level:1,         name: "getTreatment",            value:"closed", display:"none"},
-      {level:2,        name: "treatmentTypes",         value:"closed", display:"none"},
-      {level:2,        name: "treatmentTopics",        value:"closed", display:"none"},
-    {level:1,         name: "getTools",                value:"closed", display:"none"},
-    {level:1,         name: "getTraining",             value:"closed", display:"none"},
-    {level:1,         name: "getInvolved",             value:"closed", display:"none"}
-    */
   ];
-    localStorage.setItem("buttonArray", JSON.stringify(buttonArray));
+    sessionStorage.setItem("buttonArray", JSON.stringify(buttonArray));
     //console.log("ButtonArray Test " + buttonArray);
 }
 
 
+
 // END MODULE 1 x x x x x x x x x x x x x x x x x x
+
 
 
 // MODULE 2 DISTRIBUTE - - - - - - - - - - - - - - - - - - - - -
@@ -105,26 +67,41 @@ var buttonArrayLength = buttonArray.length   ;
 
 
 for (var i = 0; i < buttonArrayLength ; i++) {
-  // Get array object property name
 
+  // Get array object property name
   var buttonElementId = buttonArray[i].name;
 
+
   var buttonElement = document.getElementById(buttonArray[i].name);
+
 
   var buttonBarElement = document.getElementById("bar-" + buttonArray[i].name);
 
 
   var divDropdownElement = document.getElementById("dropdown-" + buttonElementId);
-  console.log("divDropdownElement " + divDropdownElement.id);
+
+  console.log("buttonElement " + buttonElement.id);
 
   divDropdownElement.style.display = buttonArray[i].display;
 
+
   document.getElementById(buttonBarElement.id).addEventListener("click", openClose);
+
+  buttonBarElement.setAttribute('data-buttonValue', buttonArray[i].value);
+
+  console.log("buttonArray[i].state " + buttonArray[i].state);
+
+  buttonElement.innerHTML = buttonArray[i].state;
+
+
+
 
 }
 
 
+
 // END MODULE 2 x x x x x x x x x x x x x x x x x x
+
 
 
 // MODULE 3 OPEN CLOSE - - - - - - - - - - - - - - - - - - - - -
@@ -143,6 +120,8 @@ function openClose() {
     // This removes "bar-" prefext to get root name
     var thisSubString = this.id.substring(4);
 
+    console.log("thisSubString " + thisSubString);
+
     // This searchs the array for the name
     var index = buttonArray.findIndex(x => x.name === thisSubString);
 
@@ -156,9 +135,8 @@ function openClose() {
 
     var buttonIconElement = document.getElementById(thisSubString);
 
-    if (buttonArray[i].drop) {
-      buttonIconElement.innerHTML = "–";
-    }
+    buttonIconElement.innerHTML = "–";
+    buttonArray[index].state = "–";
 
 
 
@@ -174,26 +152,21 @@ function openClose() {
     buttonArray[index].display = "none";
 
 
-    //localStorage.setItem("buttonArray", buttonArray);
+    //sessionStorage.setItem("buttonArray", buttonArray);
     var dropdown = document.getElementById("dropdown-" + thisSubString);
     dropdown.style.display= "none";
 
     var buttonIconElement = document.getElementById(thisSubString);
 
-    if (buttonArray[i].drop) {
     buttonIconElement.innerHTML = "+";
-    }
-
 
   }
-  localStorage.setItem("state", "changed");
-  localStorage.setItem("buttonArray", JSON.stringify(buttonArray));
+  sessionStorage.setItem("state", "changed");
+  sessionStorage.setItem("buttonArray", JSON.stringify(buttonArray));
 
 }
 
 // END MODULE 3 x x x x x x x x x x x x x x x x x x
-
-
 
 
 
@@ -202,47 +175,47 @@ function openClose() {
 
 
 
+// This creates a prefix so the URL in the buttonArray isn't long - can be swapped with different URL when moved
+var urlx = "file:///Users/jamesoconnell/Desktop/javascript_experiments/reference-mimo/kyh2o_menu_optimize/";
+
+var linkArray = [
+  {level:1,         name: "get-tested",    url: urlx + "get-tested.html" },
+    {level:2,       name: "glossary",      url: urlx + "glossary.html"},
+      {level:3,     name: "primary-contaminants",  url: urlx + "primary-contaminants.html"}
+]
+
+
+
+
+
+
 function expandMenu () {
-  var buttonArrayLength1 = buttonArray.length;
+  var linkArrayLength1 = linkArray.length;
   var pageURL = window.location.href;
 
 
+
   // This for loop searches the buttonArray for a stored URL that matches the URL of the loaded page
-  for (var i = 0; i < buttonArrayLength1; i++) {
+  for (var i = 0; i < linkArrayLength1; i++) {
 
     // The condition looks for a buttonArray page URL match
-    if (buttonArray[i].url === pageURL) {
+    if (linkArray[i].url === pageURL) {
 
       // This lets you know it found one
       console.log("Yes, Found One");
 
-      // This gets the button element
-      var divButtonElement = document.getElementById(buttonArray[i].name);
-      // This gets the dropdown element
-      var divBarElement = document.getElementById("bar-" + buttonArray[i].name);
-      // This gets the Level array property
-      var divLinkElement = document.getElementById("link-" + buttonArray[i].name);
-      // This gets the Level array property
+      var linkElement = document.getElementById("link-" + linkArray[i].name);
+      linkElement.style.textDecoration = "underline";
+      linkElement.style.fontWeight = "bold";
+
       var level = buttonArray[i].level;
-
-      console.log("divLinkElement" + divLinkElement.id);
-
-      divBarElement.setAttribute('data-buttonValue', 'open');
-
-      divLinkElement.style.textDecoration = "underline";
-      divLinkElement.style.fontWeight = "bold";
-
-      //if (buttonArray[i].drop) {
-      //  divButtonElement.innerHTML = "–";
-      //}
-
 
 
 
       if (level >= 2) { // - - - - - - - - - - - - -  - - - - - - - - - - - - -
 
         // This gets the Parent Bar element
-        var parentDropdown1 = document.getElementById(divBarElement.id).parentElement;
+        var parentDropdown1 = document.getElementById(linkElement.id).parentElement;
 
 
         // This removes "dropdown-" to get the button id
@@ -251,6 +224,7 @@ function expandMenu () {
 
         // This searches the buttonArray to get the index
         var index = buttonArray.findIndex(x => x.name === parentDropdown1subString);
+
 
         // This updates the record in the buttonArray
         buttonArray[index].value = "open";
@@ -261,7 +235,7 @@ function expandMenu () {
         parentDropdown1Div.style.display = "block";
 
         // Update data-buttonValue
-        // This gets the bar- Id of the element
+        //This gets the bar- Id of the element
         var parentBar1Id = "bar-" + parentDropdown1subString;
         // This gets the bar- element
         var parentBar1Element = document.getElementById(parentBar1Id);
@@ -272,12 +246,11 @@ function expandMenu () {
         var parentIcon1Id = parentDropdown1subString;
         var parentIcon1Element = document.getElementById(parentIcon1Id);
 
-        //if (buttonArray[i].drop) {
-        //  parentIcon1Element.innerHTML = "–";
-        //}
-
+        parentIcon1Element.innerHTML = "–";
+        buttonArray[index].state = "–";
 
       } // End if
+
 
       if (level >= 3) { // - - - - - - - - - - - - -  - - - - - - - - - - - - -
 
@@ -312,14 +285,12 @@ function expandMenu () {
         var parentIcon2Id = parentDropdown2subString;
         var parentIcon2Element = document.getElementById(parentIcon2Id);
 
-        //if (buttonArray[i].drop) {
-        //parentIcon2Element.innerHTML = "–";
-        //}
-
+        parentIcon2Element.innerHTML = "–";
+        buttonArray[index].state = "–";
       }
 
-      if (level >= 4) { // - - - - - - - - - - - - -  - - - - - - - - - - - - -
 
+      if (level >= 4) { // - - - - - - - - - - - - -  - - - - - - - - - - - - -
 
         // This updates the data-ButtonValue of the child
         var divBarElementChild = document.getElementById(parentDropdown2subString);
@@ -335,7 +306,7 @@ function expandMenu () {
 
         // This opens the parent dropdown div
         var parentDropdown3Div = document.getElementById(parentDropdown3.id);
-        parentDropdown2Div.style.display = "block";
+        parentDropdown3Div.style.display = "block";
 
         // This gets the bar- Id of the element
         var parentBar3Id = "bar-" + parentDropdown3subString;
@@ -348,11 +319,10 @@ function expandMenu () {
         var parentIcon3Id = parentDropdown3subString;
         var parentIcon3Element = document.getElementById(parentIcon3Id);
 
-        //if (buttonArray[i].drop) {
-        //parentIcon3Element.innerHTML = "–";
-        //}
-
+        parentIcon3Element.innerHTML = "–";
+        buttonArray[index].state = "–";
       }
+
 
       if (level >= 5) { // - - - - - - - - - - - - -  - - - - - - - - - - - - -
 
@@ -381,11 +351,8 @@ function expandMenu () {
         var parentIcon4Id = parentDropdown4subString;
         var parentIcon4Element = document.getElementById(parentIcon4Id);
 
-        //if (buttonArray[i].drop) {
-        //parentIcon4Element.innerHTML = "–";
-        //}
-
-
+        parentIcon4Element.innerHTML = "–";
+        buttonArray[index].state = "–";
       }
 
       if (level >= 6) { // - - - - - - - - - - - - -  - - - - - - - - - - - - -
@@ -423,34 +390,41 @@ function expandMenu () {
         var parentIcon5Id = parentDropdown5subString;
         var parentIcon5Element = document.getElementById(parentIcon5Id);
 
-        //if (buttonArray[i].drop) {
-        //parentIcon5Element.innerHTML = "–";
-        //}
+        parentIcon5Element.innerHTML = "–";
+        buttonArray[index].state = "–";
 
       }
 
       if (level = 1) { // - - - - - - - - - - - - - - - - - - - - - - - - - -
 
         // This updates the data-ButtonValue of the child
-        var L1Button = document.getElementById(divButtonElement.id);
+        var L1Bar = document.getElementById("bar-indoor");
+        L1Bar.setAttribute('data-buttonValue', 'open');
 
-        L1Button.setAttribute('data-buttonValue', 'open');
-        var L1Dropdown = document.getElementById("dropdown-" + divButtonElement.id);
-
+        var L1Dropdown = document.getElementById("dropdown-indoor");
         L1Dropdown.style.display = "block";
-        buttonArray[i].name.value = "open";
-        buttonArray[i].name.display = "block";
+
+        buttonArray[0].name.value = "open";
+        buttonArray[0].name.display = "block";
+
+        var L1BarSubString = L1Bar.id.substring(4);
+        console.log("L1BarSubString " + L1BarSubString);
+
+        var L1Icon = document.getElementById(L1BarSubString);
+
+        L1Icon.innerHTML = "–";
+        buttonArray[0].state = "–";
       }
 
-    }
+
+
+    } // End Main If
     else {
-      //XX
-      var urlElement = document.getElementById(buttonArray[i].name);
-      var dataAttribute = urlElement.getAttribute('data-buttonValue');
+
     }
 
-    localStorage.setItem("state", "changed");
-    localStorage.setItem("buttonArray", JSON.stringify(buttonArray));
+    sessionStorage.setItem("state", "changed");
+    sessionStorage.setItem("buttonArray", JSON.stringify(buttonArray));
 
   }
   // End For Loop
@@ -458,39 +432,3 @@ function expandMenu () {
 // End Function
 
 expandMenu ();
-
-
-
-// END MODULE 5 x x x x x x x x x x x x x x x x x x
-
-
-
-// MODULE 4 COLLAPSE MENU - - - - - - - - - - - - - - - - - - - - -
-//This collapses the whole menu as well as updates the butonArray and local storage to maintain menu state.
-
-/* BLOCK
-function collapse() {
-var buttonArrayLength1 = buttonArray.length;
-console.log("buttonArrayLength1 " + buttonArrayLength1);
-
-for (var i = 0; i < buttonArrayLength1; i++) {
-  buttonArray[i].display = "none";
-  buttonArray[i].value = "closed";
-  var buttonElementId1 = document.getElementById("dropdown-" + buttonArray[i].name);
-  console.log("buttonElementId1 " + buttonElementId1);
-  buttonElementId1.style.display = "none";
-
-  console.log("buttonElementId1 " + buttonElementId1.style.display);
-
-  document.getElementById(buttonArray[i].name).value = "closed";
-
-  var valuexx = document.getElementById(buttonArray[i].name).value;
-  console.log(buttonArray[i].name + ": " + valuexx);
-}
-localStorage.setItem("buttonArray", JSON.stringify(buttonArray));
-
-}
-
-BLOCK */
-
-// END MODULE 4 x x x x x x x x x x x x x x x x x x
