6acc3dcfc20194e630753a02e20104e03f22e361
Jim OConnell <resourcemedia@gmail.com>
2021-05-30 17:22:29 -0400

    Update

--- jsmenu.js
+++ jsmenu.js
@@ -339,9 +339,9 @@ var linkArray = [
         {level:6,     name: "broken-water-line",                      url: urlx + "/indoor-6/broken-water-line"},
 
     // Standards
-    {level:4,       name: "primary-standards",                        url: urlx + "/indoor-3/primary-standards"},
-    {level:4,       name: "secondary-standards",                      url: urlx + "/indoor-3/secondary-standards"},
-    {level:4,       name: "no-standards",                             url: urlx + "/indoor-3/no-standards"},
+    {level:4,       name: "primary-standards",                        url: urlx + "/indoor-4/primary-standards"},
+    {level:4,       name: "secondary-standards",                      url: urlx + "/indoor-4/secondary-standards"},
+    {level:4,       name: "no-standards",                             url: urlx + "/indoor-4/no-standards"},
 
 
 ];
