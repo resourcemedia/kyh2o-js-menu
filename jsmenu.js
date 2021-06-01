69b59a689e054560a4f7f24ab367d7bf0da6f156
Jim OConnell <resourcemedia@gmail.com>
2021-06-01 17:17:01 -0400

    Update

--- jsmenu.js
+++ jsmenu.js
@@ -79,6 +79,7 @@ else {
       {level:2,       name: "get-treatment",                    value:"closed",  display:"none", state: "–"},
         {level:3,     name: "treatment-types",                  value:"closed",  display:"none", state: "–"},
         {level:3,     name: "treatment-topics",                 value:"closed",  display:"none", state: "–"},
+      {level:2,       name: "get-tools",                        value:"closed",  display:"none", state: "–"},
 
   ];
     sessionStorage.setItem("buttonArray", JSON.stringify(buttonArray));
