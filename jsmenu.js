ba7cd7b487f55dece581adfc7e81ccf8b8609116
Jim OConnell <resourcemedia@gmail.com>
2021-06-01 18:27:51 -0400

    Update

--- jsmenu.js
+++ jsmenu.js
@@ -81,6 +81,7 @@ else {
         {level:3,     name: "treatment-topics",                 value:"closed",  display:"none", state: "–"},
       {level:2,       name: "get-tools",                        value:"closed",  display:"none", state: "–"},
       {level:2,       name: "get-training",                     value:"closed",  display:"none", state: "–"},
+      {level:2,       name: "get-involved",                     value:"closed",  display:"none", state: "–"},
 
   ];
     sessionStorage.setItem("buttonArray", JSON.stringify(buttonArray));
@@ -503,6 +504,11 @@ var linkArray = [
       {level:3,       name: "citizen-monitoring-and-training",         url: urlx + "/indoor-3/citizen-monitoring-and-training"},
       {level:3,       name: "classroom-and-field-training",            url: urlx + "/indoor-3/classroom-and-field-training"},
       {level:3,       name: "online-education-resources",              url: urlx + "/indoor-3/online-education-resources"},
+
+    // Get Tools
+    {level:3,         name: "get-involved-intro",                      url: urlx + "/indoor-2/get-involved"},
+      {level:3,       name: "share-your-data",                         url: urlx + "/indoor-3/share-your-data"},
+      {level:3,       name: "host-a-workshop",                         url: urlx + "/indoor-3/host-a-workshop"},
 
 ];
 
